ca2d8e729c4aa57faa887f3a8f202d2e492e627c
ReaziS <kost9n98@yandex.ru>
2019-09-23 15:29:57 +0300

    fest: add reverce. Final solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let letterArr = [];
+    let reverceMorse = {};
+    for (let name in MORSE_TABLE) {
+        reverceMorse[Object.values(MORSE_TABLE[name])] = name;
+    }
+    for(let letter in reverceMorse) {
+        let zeros = 10 - reverceMorse[letter].length *2 ;
+        let str = '0';
+        str = str.repeat(zeros);
+        reverceMorse[letter] = str + reverceMorse[letter].split('').map(item => {
+            return item == '.' ? item =  '10' : item =  '11';
+        }).join('');
+    }  
+    reverceMorse[' '] = '**********';
+    console.log(reverceMorse);
+    for(let i = 0; i < expr.length; i = i + 10){
+        letterArr.push(expr.slice(i, i+ 10));
+    }
+    let result = '';
+    
+        letterArr.map(item => {
+            for (let i in reverceMorse) {
+                if (reverceMorse[i] == item)  {
+                    result += i;
+                }
+            }
+        });
+        return result;
+    
 
+
+}
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'));
 module.exports = {
     decode
 }
